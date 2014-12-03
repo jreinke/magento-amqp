35d3bb5b3d910f13a390a69bc510ac811d6e7b3a
Johann Reinke <johann.reinke@gmail.com>
2014-12-03 08:14:36 +0100

    Tag v1.0.1

--- app/code/community/Bubble/AMQP/Helper/Broker.php
+++ app/code/community/Bubble/AMQP/Helper/Broker.php
@@ -6,8 +6,8 @@
  *
  * @category    Bubble
  * @package     Bubble_AMQP
- * @version     1.0.0
- * @copyright   Copyright (c) 2013 BubbleCode (http://shop.bubblecode.net)
+ * @version     1.0.1
+ * @copyright   Copyright (c) 2014 BubbleShop (https://www.bubbleshop.net)
  *
  * Usage:
  * <code>
--- app/code/community/Bubble/AMQP/Helper/Data.php
+++ app/code/community/Bubble/AMQP/Helper/Data.php
@@ -2,8 +2,8 @@
 /**
  * @category    Bubble
  * @package     Bubble_AMQP
- * @version     1.0.0
- * @copyright   Copyright (c) 2013 BubbleCode (http://shop.bubblecode.net)
+ * @version     1.0.1
+ * @copyright   Copyright (c) 2014 BubbleShop (https://www.bubbleshop.net)
  */
 class Bubble_AMQP_Helper_Data extends Mage_Core_Helper_Abstract
 {}
